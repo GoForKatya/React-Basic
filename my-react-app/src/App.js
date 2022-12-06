9c4d2e1d3c5cb18cba12960bf86b6613a1ef0490
GoForKatya <113207853+GoForKatya@users.noreply.github.com>
2022-12-06 13:15:23 -0600

    Learning the flow of the components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import Header from './components/Header';
 import Main from  './components/Main';
-import Sidebar from './components/Sidebar';
 import './App.css';
+import './index.css';
+import avatar from '/logo192.png';
 
+function pic(props){
+  const userPic = <img src={avatar}/>;
+  return userPic;
+}
 function App() {
   return (
     <div>
       <Header name="Anna" color="puprle"/>
       <Main greet="What's up!"/>
-      <Sidebar greet="Hey"/>
     </div>
   );
 };
--- my-react-app/src/components/Sidebar.js
+++
@@ -1,5 +0,0 @@
-function Sidebar(props){
-    return <h3>{props.greet} from Side bar </h3>
-}
-
-export default Sidebar
